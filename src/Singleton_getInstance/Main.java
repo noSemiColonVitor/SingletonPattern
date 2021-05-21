7ea7243dc7c2c97bc081b198b4ad838d77672ad0
Vitor Jorge <vitor@nosemicolon.com>
2021-05-20 22:59:07 -0700

    First Commit

---
+++ src/Singleton_getInstance/Main.java
@@ -0,0 +1,29 @@
+package Singleton_getInstance;// https://www.geeksforgeeks.org/singleton-class-java/
+
+class Main {
+    public static void main(String args[]) {
+        // instantiating Singleton_getInstance.Singleton class with variable x
+        Singleton x = Singleton.getInstance();
+
+        // instantiating Singleton_getInstance.Singleton class with variable y
+        Singleton y = Singleton.getInstance();
+
+        // instantiating Singleton_getInstance.Singleton class with variable z
+        Singleton z = Singleton.getInstance();
+
+        // changing variable of instance x
+        x.s = (x.s).toUpperCase();
+
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+        System.out.println("\n");
+
+        // changing variable of instance z
+        z.s = (z.s).toLowerCase();
+
+        System.out.println("String from x is " + x.s);
+        System.out.println("String from y is " + y.s);
+        System.out.println("String from z is " + z.s);
+    }
+}
---
+++ src/Singleton_getInstance/Singleton.java
@@ -0,0 +1,23 @@
+package Singleton_getInstance;
+
+class Singleton {
+    // static variable single_instance of type Singleton_getInstance.Singleton
+    private static Singleton single_instance = null;
+
+    // variable of type String
+    public String s;
+
+    // private constructor restricted to this class itself
+    private Singleton() {
+        s = "Hello I am a string part of Singleton_getInstance.Singleton class";
+    }
+
+    // static method to create instance of Singleton_getInstance.Singleton class
+    public static Singleton getInstance() {
+        if (single_instance == null)
+            single_instance = new Singleton();
+
+        return single_instance;
+    }
+}
+  
